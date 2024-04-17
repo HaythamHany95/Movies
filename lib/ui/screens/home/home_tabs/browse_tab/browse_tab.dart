d8453e6e23449e5c3b79c17c4e9e78902894a341
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-04-17 13:09:49 +0200

    create browse category

--- lib/ui/screens/home/home_tabs/browse_tab/browse_tab.dart
+++ lib/ui/screens/home/home_tabs/browse_tab/browse_tab.dart
@@ -1,12 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:movies/ui/screens/home/home_tabs/browse_tab/components/custom_movie.dart';
 
 class BrowseTab extends StatelessWidget {
-  const BrowseTab({super.key});
+  const BrowseTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellowAccent,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'Browse Category',
+            style: TextStyle(color: Colors.white, fontSize: 28),
+          ),
+          Expanded(
+            child: GridView.builder(
+                itemCount: 20,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 30,
+                    childAspectRatio: 1.4,
+                    mainAxisSpacing: 40),
+                itemBuilder: (context, index) {
+                  return const CustomMovie();
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
